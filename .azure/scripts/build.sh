b36bbff990f2be91f308e4dce48b54292ae46e7b
Kun-Lu <kun.lu@ibm.com>
2021-12-20 12:51:20 -0500

    Add s390x support to Azure CI and docker images

--- .azure/scripts/build.sh
+++ .azure/scripts/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export DOCKER_ORG=${DOCKER_ORG:-strimzi-test-clients}
+export DOCKER_ORG=${DOCKER_ORG:-kunlu20}
 export DOCKER_REGISTRY=${DOCKER_REGISTRY:-quay.io}
 
 echo "Build reason: ${BUILD_REASON}"
