e78519e27d05cb485a803268c2e8f3e3fe5962a2
Kun-Lu <kun.lu@ibm.com>
2022-01-05 22:07:58 -0500

    Build multi-arch images

--- docker-images/build-images.sh
+++ docker-images/build-images.sh
@@ -3,6 +3,7 @@
 KAFKA_VERSIONS=$(cat docker-images/kafka.version)
 KAFKA_MODULES=$(ls -d kafka/*/ | grep -Ev ".*/target/")
 HTTP_MODULES=$(ls -d http/*/ | grep -Ev ".*/target/")
+ARCHITECTURES="amd64 s390x"
 MODE=${1:-"push"}
 DOCKER_TAG=${DOCKER_TAG:-"latest"}
 MVN_ARGS=${MVN_ARGS:-""}
@@ -16,7 +17,10 @@ do
     if [ $MODE = "build" ];
       then
         MVN_ARGS="$MVN_ARGS -Dkafka.version=$KAFKA_VERSION" make java_build --directory=$KAFKA_MODULE
-        DOCKER_TAG="$DOCKER_TAG-kafka-$KAFKA_VERSION" make docker_build --directory=$KAFKA_MODULE
+        for ARCH in $ARCHITECTURES
+        do
+          DOCKER_ARCHITECTURE=$ARCH DOCKER_TAG="$DOCKER_TAG-kafka-$KAFKA_VERSION" make docker_build --directory=$KAFKA_MODULE
+        done
       else
         DOCKER_TAG="$DOCKER_TAG-kafka-$KAFKA_VERSION" MVN_ARGS="$MVN_ARGS-Dkafka.version=$KAFKA_VERSION" make docker_push --directory=$KAFKA_MODULE
     fi
@@ -28,7 +32,10 @@ do
   if [ $MODE = "build" ];
   then
     make java_build --directory=$HTTP_MODULE
-    make docker_build --directory=$HTTP_MODULE
+    for ARCH in $ARCHITECTURES
+    do
+      DOCKER_ARCHITECTURE=$ARCH make docker_build --directory=$HTTP_MODULE
+    done
   else
     make docker_push --directory=$HTTP_MODULE
   fi
